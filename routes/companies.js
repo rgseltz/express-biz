5f0fd741641847400667627110ac9bda2db2881f
ryan <ryanjgecelter@gmail.com>
2022-07-07 18:10:19 +0300

    new feature: GET '/companies/:code'

--- routes/companies.js
+++ routes/companies.js
@@ -3,10 +3,22 @@ const db = require('../db');
 const router = express.Router();
 const ExpressError = require('../expressError');
 
+/** gets all companies */
 router.get('/', async (req, res, next) => {
 	try {
 		const results = await db.query(`SELECT * FROM companies;`);
 		return res.status(200).json({ companies: results.rows });
+	} catch (err) {
+		next(err);
+	}
+});
+
+/** get a single company */
+router.get('/:code', async (req, res, next) => {
+	try {
+		const { code } = req.params;
+		const results = await db.query(`SELECT * FROM companies WHERE code=$1;`, [ code ]);
+		res.status(200).json({ company: results.rows[0] });
 	} catch (err) {
 		next(err);
 	}
